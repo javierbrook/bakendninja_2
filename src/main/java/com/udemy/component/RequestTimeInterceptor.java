098ab0d00c1eeab763e88015d54494495360e1b5
Javier Brook <78798686+javierbrook@users.noreply.github.com>
2022-01-30 19:36:27 +0100

    Revert "Guardando los de petición en BD"
    This reverts commit 1bd4bb12c10e54353cef13da84b2b3f06bcaf7c3.

--- src/main/java/com/udemy/component/RequestTimeInterceptor.java
+++ src/main/java/com/udemy/component/RequestTimeInterceptor.java
@@ -1,26 +1,17 @@
 package com.udemy.component;
 
-import java.util.Date;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
-import com.udemy.repository.LogRepository;
-
-import lombok.extern.java.Log;
-
-@Log
 @Component("requestTimeInterceptor")
 public class RequestTimeInterceptor extends HandlerInterceptorAdapter  {
 
-	@Autowired
-	private LogRepository logRepository;
+	private static final Log LOG = LogFactory.getLog(RequestTimeInterceptor.class);
 	
 	//Se ejecuta 1ro, cuando comeienza la petición
 	@Override
@@ -34,18 +25,7 @@ public class RequestTimeInterceptor extends HandlerInterceptorAdapter  {
 	@Override
 	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
 			throws Exception {
-		
-		String url = request.getRequestURL().toString();
-		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-		String username = "";
-		if(auth != null) {
-			if(auth.isAuthenticated()) {
-				username = auth.getName();
-			}
-			logRepository.save(new com.udemy.entity.Log(new Date(), auth.getDetails().toString(), username, url)); 
-		}
-		
-		log.info("--REQUEST URL: '"+url+"' --TOTAL TIME: '"+(System.currentTimeMillis() - (long)request.getAttribute("startTime"))+"'ms" );
+		LOG.info("--REQUEST URL: '"+request.getRequestURL().toString()+"' --TOTAL TIME: '"+(System.currentTimeMillis() - (long)request.getAttribute("startTime"))+"'ms" );
 	}
 	
 }
